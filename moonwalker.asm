58cf156278c0ad4248b1a465b91c2b8f05b74057
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-10 18:56:42 +0100

    added cart info

--- moonwalker.asm
+++ moonwalker.asm
@@ -12,3 +12,14 @@ CARTRIDGE_INIT:
 ;; SET REV TO 1 FOR THE WORLD VERSION (NTSC AND JPN)
 
 GAME_REV:            EQU          0
+
+DC.B                 "SEGA MEGA DRIVE "                            ;; CONSOLE NAME
+DC.B                 "(C)SEGA 1990.JUL"                            ;; RELEASE DATE
+DC.B                 "MICHAEL JACKSON          FS MOONWALKER"      ;; DOMESTIC NAME
+
+if GAME_REV=0
+
+DC.B "GM 00004028-01"                                              ;; GAME VERSION - DOMESTIC
+else 
+
+DC.B "GM 00004048-00"                                              ;; GAME VERSION - NON-DOM
