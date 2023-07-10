577041170caffd8096a0eb707e877648788c123f
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-10 17:33:34 +0100

    Update macros.asm

--- macros.asm
+++ macros.asm
@@ -26,9 +26,8 @@ SUPPORT_VSRAM:      MACRO
 @LOOP\@             if ENABLE_VSRAM = 1
                     DC.B $2A3F + VSRAM_ADDR << 3, $20                     ;; ASSUME THE LENGTH OF THE
 					                                  ;; VSRAM CONSTANT, ACCESS THE ADDRESS IN PROPORTION TO THAT
-
                     else 
-					
+
                     DC.L $20202020                    ;; ENABLE THE VSRAM ACCORDINGLY
                     DC.L $20202020                    ;; AND DEFINE WHICH ADDRESS IT SHOULD
                     DC.B "       "                    ;; EXECUTE IT'S RUNTIME AT
@@ -46,10 +45,17 @@ BUS_ERROR:          MACRO
                     @LOOP\@
                     ENDM
 
+;; CREATES AN ADDRESS DIFFERENCE TO DISCERN BETWEEN VALUES ON GPR'S 14 AND 16
+;; 
 
+VDP_ADDR_DIF:      VALUE, ADDR,((ADDR &$3FFF) << 16) || ((ADDR $0000) >> 14) 
 
 DMA_VDP:            MACRO SOURCE, DEST, LEN, TYPE
-                    LEA VDP_CONTROL.L, A5   ;; LOAD THE EFFECTIVE ADDRESS OF THE VDP CONTROL IN GPR A5
+                    LEA $C00004.L, A5   ;; LOAD THE EFFECTIVE ADDRESS OF THE VDP CONTROL IN GPR A5 
+                                        ;; SEE https://segaretro.org/Sega_Mega_Drive/VDP_general_usage#Registers
+
+                    LEA $C00000.L, A6
+                    MOVE.W $0002, A5                    
                     ENDM
 
 ;----------------------------------------------------------
